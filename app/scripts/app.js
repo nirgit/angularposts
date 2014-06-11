0e4a15e197522d8c1439e44ee8d2e43ea3815826
Nir Moav <nirm@wix.com>
2014-06-11 20:29:55 +0300

    setting up a controller and a view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
+/* global app:true */
 'use strict';
 
 /**
@@ -8,26 +9,26 @@
  *
  * Main module of the application.
  */
-angular
-  .module('thinkngApp', [
+var app = angular.module('thinkngApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+]);
+
+app.config(function($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostController'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
